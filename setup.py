0ed00d50f7a3b3435fc6cae62be7b4f7209e2103
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-04-25 00:55:38 +0000

    carga inicial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="piv_2025",
+    version="0.0.1",
+    author="Andres Callejas",
+    author_email="",
+    description="",
+    py_modules=["actividad_1","actividad_2"],
+    install_requires=[
+        "pandas",
+        "numpy",
+        "openpyxl",
+        "requests"
+    ]
+) 
