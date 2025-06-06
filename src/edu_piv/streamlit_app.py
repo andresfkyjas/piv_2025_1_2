cad256d2998de7ffa9ad64ed24a5ed258285fde9
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-06-06 00:00:10 +0000

    correccion importcacion

--- src/edu_piv/streamlit_app.py
+++ src/edu_piv/streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
+#from streamlit_pandas_profiling import st_profile_report
 from ydata_profiling import ProfileReport
 import pandas as pd
 import os
@@ -13,8 +13,10 @@ def main():
 
     df = pd.read_csv("src/edu_piv/static/data/data_extractor.csv")
     profile = ProfileReport(df, title="Dashboard Indicador Dolar")
+    st.title("Análisis de Datos")
+    st.write(profile.to_html(), unsafe_allow_html=True)
 
-    st_profile_report(profile)
+    #st_profile_report(profile)
 
 
 if __name__ == "__main__":
